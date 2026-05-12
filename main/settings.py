9811b5159db305374779f099a99a53fea289e89c
a1tvv <altaiabdurahim07@gmail.com>
2026-05-12 12:29:41 +0600

    v1.4

--- main/settings.py
+++ main/settings.py
@@ -164,6 +164,9 @@ STORAGES = {
     },
 }
 
+if 'VERCEL' in os.environ:
+    DEBUG = False
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
